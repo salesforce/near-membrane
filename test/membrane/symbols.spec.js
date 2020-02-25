850a99985599c4c167ca2213ca51059772d70094
Caridy Patiño <caridy@gmail.com>
2020-02-24 23:40:59 -0500

    closes #60: adding tests to validate that symbols are not leaking references of objects from outer realm

---
+++ test/membrane/symbols.spec.js
@@ -0,0 +1,46 @@
+import createSecureEnvironment from '../../lib/browser-realm.js';
+
+globalThis.regularSymbol = Symbol();
+globalThis.symbolWithDescription = Symbol('symbol-with-desc');
+globalThis.symbolWithKey = Symbol.for('symbol-with-key');
+
+describe('Secure Membrane', () => {
+    it('should support symbols', () => {
+        // expect.assertions(5);
+        const evalScript = createSecureEnvironment();
+        evalScript(`
+            expect(typeof Symbol() === 'symbol').toBeTrue();
+            expect(typeof Symbol.for('x') === 'symbol').toBeTrue();
+            expect(Symbol.for('x') === Symbol.for('x')).toBeTrue();
+            expect(Symbol.keyFor(Symbol.for('x'))).toBe('x');
+            expect(Symbol().constructor.__proto__ === Function.prototype).toBeTrue();
+            expect(Symbol().constructor.__proto__.constructor('return this')() === globalThis).toBeTrue();
+        `);
+    });
+    it('should allow access to symbols defined in outer realm', function() {
+        // expect.assertions(3);
+        const evalScript = createSecureEnvironment();
+        evalScript(`
+            expect(typeof globalThis.regularSymbol).toBe('symbol');
+            expect(typeof globalThis.symbolWithDescription).toBe('symbol');
+            expect(typeof globalThis.symbolWithKey).toBe('symbol');
+        `);
+    });
+    it('should not leak outer realm global reference via symbols', function() {
+        // expect.assertions(2);
+        const evalScript = createSecureEnvironment();
+        evalScript(`
+            expect(globalThis.regularSymbol.constructor).toBe(Symbol);
+            expect(globalThis.regularSymbol.constructor.__proto__.constructor('return this')() === globalThis).toBeTrue();
+        `);
+    });
+    it('should not leak outer realm global reference via Symbol.for()', function() {
+        // expect.assertions(3);
+        const evalScript = createSecureEnvironment();
+        evalScript(`
+            expect(typeof Symbol.for('symbol-with-key')).toBe('symbol');
+            expect(Symbol.for('symbol-with-key')).toBe(Symbol.for('symbol-with-key'));
+            expect(Symbol.for('symbol-with-key').constructor === Symbol).toBeTrue();
+        `);
+    });
+});
