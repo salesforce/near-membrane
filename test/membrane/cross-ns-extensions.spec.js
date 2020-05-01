646555c3b69b6fdfde7a3ee75166a8609913f67b
Caridy Patiño <caridy@gmail.com>
2020-05-01 17:05:43 -0400

    fix: namespace to namespace class extensions (#84)
    * fix issue with ns-to-ns class extensions that were reporting the incorrect prototype value on constructors

---
+++ test/membrane/cross-ns-extensions.spec.js
@@ -0,0 +1,45 @@
+import createSecureEnvironment from '../../lib/browser-realm.js';
+
+class Base {
+    base() {
+        return 'from base';
+    }
+}
+let Foo;
+function saveFoo(f) {
+    Foo = f;
+}
+
+const evalScript = createSecureEnvironment(undefined, { Base, saveFoo });
+evalScript(`
+    class Foo extends Base {
+        foo() {
+            return 'from foo';
+        }
+    }
+    saveFoo(Foo);
+`);
+
+const endowments = {
+    Foo,
+    expect
+};
+
+describe('The membrane', () => {
+    it('should allow expandos on endowments inside the sandbox', function() {
+        // expect.assertions(4);
+        const evalScript = createSecureEnvironment(undefined, endowments);
+        evalScript(`
+            'use strict';
+            expect(Foo.prototype.base()).toBe('from base');
+            expect(Foo.prototype.foo()).toBe('from foo');
+            class Bar extends Foo {
+                foo() {
+                    return 'from bar as override';
+                }
+            }
+            expect(new Bar().base()).toBe('from base');
+            expect(new Bar().foo()).toBe('from bar as override');
+        `);
+    });
+});
---
+++ test/membrane/object-semantics.spec.js
@@ -0,0 +1,49 @@
+import createSecureEnvironment from '../../lib/browser-realm.js';
+
+let obj;
+function saveObject(o) {
+    obj = o;
+}
+
+const endowments = {
+    saveObject,
+    expect
+};
+
+describe('Blue Proxies', () => {
+    it('should be preserved the JS Object semantics by allowing writable objects to change', () => {
+        'use strict';
+        // expect.assertions(9);
+        const evalScript = createSecureEnvironment(undefined, endowments);
+        evalScript(`
+            'use strict';
+            const obj = {
+                mutate() {
+                    Object.defineProperty(this, 'x', {
+                        value: 3,
+                        writable: false,
+                    });
+                }
+            };
+            Object.defineProperty(obj, 'x', {
+                value: 1,
+                writable: true,
+            });
+            saveObject(obj);
+        `);
+        expect(obj.x).toBe(1);
+        expect(Object.getOwnPropertyDescriptor(obj, 'x').value).toBe(1);
+        obj.x = 2;
+        expect(obj.x).toBe(2);
+        expect(Object.getOwnPropertyDescriptor(obj, 'x').value).toBe(2);
+        obj.mutate();
+        obj.x;
+        expect(obj.x).toBe(3);
+        expect(Object.getOwnPropertyDescriptor(obj, 'x').value).toBe(3);
+        expect(() => {
+            obj.x = 4;
+        }).toThrow();
+        expect(obj.x).toBe(3);
+        expect(Object.getOwnPropertyDescriptor(obj, 'x').value).toBe(3);
+    });
+}); 
