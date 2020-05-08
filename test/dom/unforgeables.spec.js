193b1e497fd1405092458fe6f36f6cbfa5d842be
Caridy Patiño <caridy@gmail.com>
2020-05-08 18:00:29 -0400

    fix #85: global EventTarget is configurable and can be replaced after remapping its prototype to the sandbox prototype, which is unforgeable

---
+++ test/dom/unforgeables.spec.js
@@ -0,0 +1,27 @@
+import createSecureEnvironment from '../../lib/browser-realm.js';
+
+const evalScript = createSecureEnvironment(undefined, window);
+
+describe('EventTarget unforgeable', () => {
+    it('should be accessible from window', function() {
+        // expect.assertions(4);
+        evalScript(`
+            expect(EventTarget !== undefined).toBe(true);
+            expect(window.__proto__.__proto__.__proto__ === EventTarget.prototype).toBe(true);
+            expect(document.body instanceof EventTarget).toBe(true);
+            expect(document.createElement('p') instanceof EventTarget).toBe(true);
+        `);
+    });
+});
+
+describe('Window unforgeable', () => {
+    it('should be accessible from window', function() {
+        // expect.assertions(4);
+        evalScript(`
+            expect(Window !== undefined).toBe(true);
+            expect(window.__proto__ === Window.prototype).toBe(true);
+            expect(window instanceof Window).toBe(true);
+            expect(globalThis instanceof Window).toBe(true);
+        `);
+    });
+});
