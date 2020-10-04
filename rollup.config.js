19d5137a67f6b4869b2bb79c59170f9374d5a895
Ted Conn <tconn@salesforce.com>
2020-10-04 15:39:56 +0200

    feat: adding bundlesize target to test for bundlesize increases

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,23 @@
-module.exports = {
-    input: 'examples/web-components/platform.js',
-    output: {
-        file: 'examples/web-components/bundle.js',
-        format: 'cjs'
+import typescript from '@rollup/plugin-typescript'
+import { terser } from 'rollup-plugin-terser'
+
+function config(input, file, plugins) {
+    return {
+        input,
+        output: {
+            file,
+            format: 'es',
+            sourcemap: true,
+            plugins
+
+        },
+        plugins: [typescript()]
     }
 };
+
+export default [
+    config('src/index.ts', 'lib/index.js'),
+    config('src/index.ts', 'lib/index.min.js', [terser()]),
+    config('src/browser-realm.ts', 'lib/browser-realm.js'),
+    config('src/browser-realm.ts', 'lib/browser-realm.min.js', [terser()])
+];
