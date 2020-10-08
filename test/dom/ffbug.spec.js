e73688c9d0329fdbe24b65ccd836447253ae39e7
Caridy Patiño <caridy@gmail.com>
2020-10-08 15:21:54 -0400

    fixes #121: workaround for FF bug on document identity when keepAlive option is passed (#122)

---
+++ test/dom/ffbug.spec.js
@@ -0,0 +1,31 @@
+import createSecureEnvironment from '../../lib/browser-realm.js';
+
+describe('FF BugFix 543435', () => {
+    it('should preserve the document reference in the next turn', function(done) {
+        // expect.assertions(3);
+        const evalScript = createSecureEnvironment({
+            endowments: {
+                validateSyncDocumentReference(redDoc) {
+                    expect(redDoc).toBe(document);
+                },
+                validateMicroTaskDocumentReference(redDoc) {
+                    expect(redDoc).toBe(document);
+                },
+                validateMacroTaskDocumentReference(redDoc) {
+                    expect(redDoc).toBe(document);
+                    done();
+                },
+            },
+            keepAlive: true,
+        });
+        evalScript(`
+            validateSyncDocumentReference(document);
+            Promise.resolve().then(() => {
+                validateMicroTaskDocumentReference(document);   
+            });
+            setTimeout(() => {
+                validateMacroTaskDocumentReference(document); 
+            }, 1);
+        `);
+    });
+});
