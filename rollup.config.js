3a8524bbcc3671148520027c050d21a18983574b
Ted Conn <tconn@salesforce.com>
2020-10-08 21:24:48 +0200

    feat: testing for bundle size increases (#126)
    * feat: adding bundlesize target to test for bundlesize increases
    
    * fix: add bundlesize test to build
    
    * fix: slight optimization to bundling
    
    * fix: move dep to dev dep
    
    * fix: outputting a node-realm version also
    
    * fix: set sourcemaps to true always

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,28 @@
-module.exports = {
-    input: 'examples/web-components/platform.js',
-    output: {
-        file: 'examples/web-components/bundle.js',
-        format: 'cjs'
+import typescript from '@rollup/plugin-typescript'
+import { terser } from 'rollup-plugin-terser'
+
+function config(input, filePrefix, external = []) {
+    return {
+        input,
+        output: [{
+            file: `lib/${filePrefix}.js`,
+            format: 'es',
+            sourcemap: true,
+        },
+        {
+            file: `lib/${filePrefix}.min.js`,
+            format: 'es',
+            sourcemap: true,
+            plugins: [terser()]
+
+        }],
+        plugins: [typescript()],
+        external
     }
 };
+
+export default [
+    config('src/index.ts', 'index'),
+    config('src/browser-realm.ts', 'browser-realm'),
+    config('src/node-realm.ts', 'node-realm', ['vm'])
+];
