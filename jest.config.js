ff884bf37f424e71a2026b6aa0750890889e5120
Manuel Jasso <mjasso@gmail.com>
2020-12-16 17:02:38 -0800

    Jest tests pass but we still get ts-jest errors

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,17 @@
 module.exports = {
     "roots": [
-      "<rootDir>/packages/node-membrane/src"
+      "<rootDir>/packages"
     ],
-    // "transform": {
-    //   "^.+\\.tsx?$": "ts-jest"
-    // },
-    // globals: {
-    //   'ts-jest': {
-    //     diagnostics: false
-    //   }
-    // }
+    "moduleNameMapper": {
+      "^@locker/near-membrane$": "<rootDir>/packages/near-membrane/src",
+      "^@locker/node-membrane$": "<rootDir>/packages/node-membrane/src",
+    },
+    "transform": {
+      "^.+\\.tsx?$": "ts-jest"
+    },
+    globals: {
+      'ts-jest': {
+        diagnostics: false
+      }
+    }
 }
