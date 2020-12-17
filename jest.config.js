aa47f456d59ae4192ce9d386bd5826c531fb854d
Manuel Jasso <mjasso@gmail.com>
2020-12-16 17:04:29 -0800

    Jest test pass and we get no errors by removing ts-jest and using babel.config.js presets

--- jest.config.js
+++ jest.config.js
@@ -5,13 +5,5 @@ module.exports = {
     "moduleNameMapper": {
       "^@locker/near-membrane$": "<rootDir>/packages/near-membrane/src",
       "^@locker/node-membrane$": "<rootDir>/packages/node-membrane/src",
-    },
-    "transform": {
-      "^.+\\.tsx?$": "ts-jest"
-    },
-    globals: {
-      'ts-jest': {
-        diagnostics: false
-      }
     }
 }
