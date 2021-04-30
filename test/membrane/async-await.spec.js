78c3909916943f37c8474f8dba88cb74d0c71bce
rwaldron <waldron.rick@gmail.com>
2021-04-30 15:08:39 -0400

    chore: re-enable Safari for async/await test

--- test/membrane/async-await.spec.js
+++ test/membrane/async-await.spec.js
@@ -1,11 +1,7 @@
 import createVirtualEnvironment from "@locker/near-membrane-dom";
 
-// TODO #115 - Skip Firefox and Safari until we find a solution for them
-const isFirefox = navigator.userAgent.includes('Firefox/');
-const isSafari = navigator.userAgent.includes('Safari/');
-const skipTests = isFirefox || isSafari;
-
-if (!skipTests) {
+// TODO #115 - Skip Firefox ~~and Safari~~ until we find a solution for them
+if (!navigator.userAgent.includes('Firefox/')) {
     describe("async/await", () => {
         it("basic wrapping", (done) => {
             const evalScript = createVirtualEnvironment({ endowments: { done, expect }});
