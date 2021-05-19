fb8d0ffb52738e3dca557834b279994f69973634
rwaldron <waldron.rick@gmail.com>
2021-05-19 10:47:55 -0400

    fix: changes per review

--- test/__bootstrap__/3rdparty/expect.assertions.js
+++ test/__bootstrap__/3rdparty/expect.assertions.js
@@ -4,7 +4,7 @@
 
 EXPECT_PATCHING_SCOPE: {
     if (!globalThis.jasmine) {
-        throw new Error("jasmine must be loaded before patching in expect.assertions(x)");
+        throw new Error('jasmine must be loaded before patching in expect.assertions(x)');
     }
 
     let actualExpects = 0;
@@ -19,20 +19,20 @@ EXPECT_PATCHING_SCOPE: {
 
     globalThis.expect = function(...args) {
         actualExpects++;
-        return Reflect.apply(originalExpect, this, args)
+        return Reflect.apply(originalExpect, this, args);
     };
 
-    globalThis.expect.assertions = (num) => {
-        num = Number.parseInt(num);
+    globalThis.expect.assertions = (count) => {
+        const num = Number.parseInt(count);
         if (!Number.isSafeInteger(num)) {
-            throw new Error("jasmine.expectCount expects a number >= 0 as the first argument.");
+            throw new Error('expect.assertions(count) expects count to be a number >= 0 as the first argument.');
         }
         expectedExpects = num;
     };
 
     const checkExpectCount = () => {
         if (expectedExpects !== null && actualExpects !== expectedExpects) {
-            throw `Expected ${expectedExpects} expect${expectedExpects !== 1 ? "s" : ""} to be called, ${actualExpects} expect${actualExpects !== 1 ? "s were" : " was"} actually called.`;
+            throw new Error(`Expected ${expectedExpects} expect${expectedExpects !== 1 ? 's' : ''} to be called, ${actualExpects} expect${actualExpects !== 1 ? 's were' : ' was'} actually called.`);
         }
     };
 
