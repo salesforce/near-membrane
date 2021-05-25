4099d3f05f53942a6dee3defa6396cf7c3942609
rwaldron <waldron.rick@gmail.com>
2021-05-25 16:44:01 -0400

    chore: test keepAlive: true & false; add explanation of ignored coverage

---
+++ test/environment/createvirtualenvironment.spec.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line import/no-unresolved
+import createVirtualEnvironment from '@locker/near-membrane-dom';
+
+describe('createVirtualEnvironment', () => {
+    it('no options', () => {
+        const evalScript = createVirtualEnvironment();
+        expect(() => evalScript('')).not.toThrow();
+    });
+    it('empty options', () => {
+        const evalScript = createVirtualEnvironment({});
+        expect(() => evalScript('')).not.toThrow();
+    });
+    it('keepAlive: true', () => {
+        const count = window.frames.length;
+        const evalScript = createVirtualEnvironment({ keepAlive: true });
+        expect(window.frames.length).toBe(count + 1);
+        expect(() => evalScript('')).not.toThrow();
+    });
+    it('keepAlive: false', () => {
+        const evalScript = createVirtualEnvironment({ keepAlive: false });
+        expect(() => evalScript('')).not.toThrow();
+    });
+});
