dd163c0900614eeea0c383e239ccdd7aaeeeb7d5
Rick Waldron <waldron.rick@gmail.com>
2021-05-27 10:31:17 -0400

    Update test/membrane/async-await.spec.js
    Co-authored-by: John-David Dalton <john.david.dalton@gmail.com>

--- test/membrane/async-await.spec.js
+++ test/membrane/async-await.spec.js
@@ -1,6 +1,6 @@
 import createVirtualEnvironment from "@locker/near-membrane-dom";
 
-// TODO #115 - Skip Firefox ~~and Safari~~ until we find a solution for them
+// TODO #115 - Skip Firefox until we find a non-compiler based solution for them
 if (!navigator.userAgent.includes('Firefox/')) {
     describe("async/await", () => {
         it("basic wrapping", (done) => {
