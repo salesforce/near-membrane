16ac9754528015ce2e38c2751825f6db6364b089
Rick Waldron <waldron.rick@gmail.com>
2021-06-15 11:42:18 -0400

    test: @W-9323615@ near-membrane-base/src/shared.ts to 100% coverage (#164)

---
+++ packages/near-membrane-base/src/__tests__/shared.spec.ts
@@ -0,0 +1,65 @@
+import { ObjectLookupOwnGetter, WeakMapHas } from '../shared';
+
+describe('ObjectLookupOwnGetter', () => {
+    it('should return undefined when object argument is null', () => {
+        expect.assertions(1);
+        expect(ObjectLookupOwnGetter(null)).toBe(undefined);
+    });
+    it('should return undefined when object argument is undefined', () => {
+        expect.assertions(1);
+        expect(ObjectLookupOwnGetter(undefined)).toBe(undefined);
+    });
+    it('should return undefined when object argument has no such property', () => {
+        expect.assertions(1);
+        expect(ObjectLookupOwnGetter({}, 'foo')).toBe(undefined);
+    });
+    it('should return getter when object argument has property and getter', () => {
+        expect.assertions(1);
+        const o = {
+            get x() {
+                return 1;
+            },
+        };
+        expect(ObjectLookupOwnGetter(o, 'x')).toBe(Object.getOwnPropertyDescriptor(o, 'x').get);
+    });
+    it('should not lookup inherited getters', () => {
+        expect.assertions(2);
+
+        class C {
+            get foo() {
+                return this;
+            }
+        }
+
+        const c = new C();
+
+        const o = Object.create({
+            get foo() {
+                return this;
+            },
+        });
+
+        expect(ObjectLookupOwnGetter(c, 'foo')).toBe(undefined);
+        expect(ObjectLookupOwnGetter(o, 'foo')).toBe(undefined);
+    });
+});
+
+describe('WeakMapHas', () => {
+    const key = {};
+    const value = {};
+    const map = new Map([[key, value]]);
+    const weakMap = new WeakMap([[key, value]]);
+
+    it('should not work with a non-WeakMap', () => {
+        expect.assertions(1);
+
+        expect(() => {
+            WeakMapHas(map, key);
+        }).toThrow(TypeError);
+    });
+
+    it('should work with a WeakMap', () => {
+        expect.assertions(1);
+        expect(WeakMapHas(weakMap, key)).toBe(true);
+    });
+});
