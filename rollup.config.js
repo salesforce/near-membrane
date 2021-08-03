c83a95e89a41aadc292f987fe088282920513376
rwaldron <waldron.rick@gmail.com>
2021-08-03 16:00:32 -0400

    chore: run prettier auto-fix on rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,23 @@
-import typescript from '@rollup/plugin-typescript'
-import { terser } from 'rollup-plugin-terser'
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
 export function rollupConfig({ input = 'src/index.ts', filePrefix = 'index', external = [] } = {}) {
     return {
         input,
-        output: [{
-            file: `lib/${filePrefix}.js`,
-            format: 'es',
-            sourcemap: true,
-        },
-        {
-            file: `lib/${filePrefix}.min.js`,
-            format: 'es',
-            sourcemap: true,
-            plugins: [terser()]
-        }],
+        output: [
+            {
+                file: `lib/${filePrefix}.js`,
+                format: 'es',
+                sourcemap: true,
+            },
+            {
+                file: `lib/${filePrefix}.min.js`,
+                format: 'es',
+                sourcemap: true,
+                plugins: [terser()],
+            },
+        ],
         plugins: [typescript()],
-        external
+        external,
     };
-};
+}
