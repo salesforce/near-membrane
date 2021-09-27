cba963cd852a0f8d26f7fc59d553a255ed3a156b
rwaldron <waldron.rick@gmail.com>
2021-09-27 11:01:27 -0400

    chore: restrict jest to looking in <rootDir>/packages/near-membrane-base & -node

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    roots: ['<rootDir>/packages'],
+    roots: ['<rootDir>/packages/near-membrane-base', '<rootDir>/packages/near-membrane-node'],
     moduleNameMapper: {
         '^@locker/near-membrane-base$': '<rootDir>/packages/near-membrane-base/src',
         '^@locker/near-membrane-node$': '<rootDir>/packages/near-membrane-node/src',
