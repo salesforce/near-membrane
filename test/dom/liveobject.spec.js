e08b80afd39581128271a7fb4d74c544682ad915
Rick Waldron <waldron.rick@gmail.com>
2021-10-08 17:08:54 -0400

    fix: ensure set trap returns consistent result with Reflect.set, Revised (#216)
    * fix: ensure set trap returns consistent result with Reflect.set
    
    * fix: tests access and updates reflecting across realm boundary in the presence of @@lockerLiveValue
    
    Co-authored-by: John-David Dalton <john.david.dalton@gmail.com>

--- test/dom/liveobject.spec.js
+++ test/dom/liveobject.spec.js
@@ -27,4 +27,106 @@ describe('@@lockerLiveValue', () => {
         const styleAttributeValue = div.getAttribute('style');
         expect(styleAttributeValue).toBe('color: red;');
     });
+    it('from system mode class', () => {
+        expect.assertions(10);
+
+        const SYMBOL = Symbol.for('@@lockerLiveValue');
+        class X {
+            constructor() {
+                this[SYMBOL] = undefined;
+                this.foo = 0;
+            }
+
+            incrementXFromOutside() {
+                this.foo++;
+            }
+
+            getFooFromOutside() {
+                return this.foo;
+            }
+        }
+
+        const endowments = {
+            X,
+            expect,
+            createYFromOutside(Y) {
+                // eslint-disable-next-line no-new
+                new Y();
+            },
+        };
+
+        const env = createVirtualEnvironment(window, window, { endowments });
+
+        env.evaluate(`
+            class Y extends X {
+                constructor() {
+                    super();
+
+                    expect(this.getFooFromOutside()).toBe(0);
+
+                    this.foo = 9;
+
+                    expect(this.foo).toBe(9);
+                    expect(this.getFooFromOutside()).toBe(9);
+
+                    this.incrementXFromOutside();
+
+                    expect(this.foo).toBe(10);
+                    expect(this.getFooFromOutside()).toBe(10);
+                }
+            }
+            new Y();
+            createYFromOutside(Y);
+        `);
+    });
+
+    it('missing from system mode class', () => {
+        expect.assertions(10);
+        class A {
+            constructor() {
+                this.foo = 0;
+            }
+
+            incrementAFromOutside() {
+                this.foo++;
+            }
+
+            getFooFromOutside() {
+                return this.foo;
+            }
+        }
+
+        const endowments = {
+            A,
+            expect,
+            createBFromOutside(B) {
+                // eslint-disable-next-line no-new
+                new B();
+            },
+        };
+
+        const env = createVirtualEnvironment(window, window, { endowments });
+
+        env.evaluate(`
+            class B extends A {
+                constructor() {
+                    super();
+
+                    expect(this.getFooFromOutside()).toBe(0);
+
+                    this.foo = 9;
+
+                    expect(this.foo).toBe(9);
+                    expect(this.getFooFromOutside()).toBe(0);
+
+                    this.incrementAFromOutside();
+
+                    expect(this.foo).toBe(9);
+                    expect(this.getFooFromOutside()).toBe(1);
+                }
+            }
+            createBFromOutside(B);
+            new B();
+        `);
+    });
 });
