9e9b2e4736c9b8280e4dcd4f79ec51a6a5a8b60f
John-David Dalton <john.david.dalton@gmail.com>
2021-10-18 15:13:38 -0400

    feat: add dev_mode conditional compilation constant (#221)

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,40 @@
-import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+'use strict';
 
-export function rollupConfig({ input = 'src/index.ts', filePrefix = 'index', external = [] } = {}) {
+const replacePlugin = require('@rollup/plugin-replace');
+const { terser } = require('rollup-plugin-terser');
+const typescriptPlugin = require('@rollup/plugin-typescript');
+
+function createConfig({
+    input = 'src/index.ts',
+    filePrefix = 'index',
+    external = [],
+    prod = false,
+} = {}) {
+    const DEV_MODE = !prod;
     return {
         input,
-        output: [
-            {
-                file: `lib/${filePrefix}.js`,
-                format: 'es',
-                sourcemap: true,
-            },
-            {
-                file: `lib/${filePrefix}.min.js`,
-                format: 'es',
-                sourcemap: true,
-                plugins: [terser()],
-            },
+        output: {
+            file: `lib/${filePrefix}${prod ? '.min' : ''}.js`,
+            format: 'es',
+            sourcemap: true,
+            plugins: [prod ? terser() : undefined],
+        },
+        plugins: [
+            typescriptPlugin(),
+            replacePlugin({
+                preventAssignment: true,
+                DEV_MODE: JSON.stringify(DEV_MODE),
+            }),
         ],
-        plugins: [typescript()],
         external,
     };
 }
+
+module.exports = {
+    rollupConfig(options = {}) {
+        return [
+            createConfig({ ...options, prod: false }),
+            createConfig({ ...options, prod: true }),
+        ];
+    },
+};
