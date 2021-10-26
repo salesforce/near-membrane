e0cad33c2e49cdd9b7286b1ec53425e0e208c986
Gary Chang <gary.chang@salesforce.com>
2021-10-25 22:24:19 -0700

    fix: add test for instrumentation

---
+++ test/membrane/instrumentation.spec.js
@@ -0,0 +1,44 @@
+import createVirtualEnvironment from '@locker/near-membrane-dom';
+
+describe('Ensure instrumentation object usage in near-membrane', () => {
+    let redProxy;
+    const endowments = {
+        setter(v) {
+            redProxy = v;
+        },
+    };
+    const mockInstrumentation = {
+        startActivity: () => ({ stop: () => {}, error: () => {} }),
+        log: () => {},
+        error: () => {},
+    };
+    const code = `const foo = {foo: 'bar', fn: function(v) {}};
+setter(foo);`;
+
+    it('Instrumentation object is passed to and used in near-membrane', () => {
+        const startActivitySpy = spyOn(mockInstrumentation, 'startActivity').and.callThrough();
+        const logSpy = spyOn(mockInstrumentation, 'log').and.callThrough();
+        const errorSpy = spyOn(mockInstrumentation, 'error').and.callThrough();
+        const ve = createVirtualEnvironment(window, window, {
+            endowments,
+            instrumentation: mockInstrumentation,
+        });
+
+        ve.evaluate(code);
+
+        redProxy.foo;
+        expect(startActivitySpy).toHaveBeenCalledTimes(2);
+        expect(logSpy).not.toHaveBeenCalled();
+        expect(errorSpy).not.toHaveBeenCalled();
+
+        redProxy.foo = 1;
+        expect(startActivitySpy).toHaveBeenCalledTimes(4);
+        expect(logSpy).not.toHaveBeenCalled();
+        expect(errorSpy).not.toHaveBeenCalled();
+
+        redProxy.fn();
+        expect(startActivitySpy).toHaveBeenCalledTimes(6);
+        expect(logSpy).not.toHaveBeenCalled();
+        expect(errorSpy).not.toHaveBeenCalled();
+    });
+});
