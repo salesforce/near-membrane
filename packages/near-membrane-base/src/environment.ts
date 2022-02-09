b7eaa30bfd40af8e2055169aa0a948b3fb3ae886
John-David Dalton <john.david.dalton@gmail.com>
2022-02-09 12:10:08 -0800

    chore: minor typo fix (#264)

--- packages/near-membrane-base/src/environment.ts
+++ packages/near-membrane-base/src/environment.ts
@@ -28,7 +28,7 @@ interface VirtualEnvironmentOptions {
     redConnector: ReturnType<typeof createMembraneMarshall>;
     // Environment support object
     support?: SupportFlagsObject;
-    // Instrumentation libray object
+    // Instrumentation library object
     instrumentation?: InstrumentationHooks;
 }
 
