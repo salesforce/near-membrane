3ba3bc41fbd49bc7331629646dc3579143be8211
jdalton <john.david.dalton@gmail.com>
2022-06-21 12:38:53 -0700

    feat: use unforgibles for connector cache

--- packages/near-membrane-base/src/connector.ts
+++ packages/near-membrane-base/src/connector.ts
@@ -1,18 +1,8 @@
 import { createMembraneMarshall } from './membrane';
-import { toSafeWeakMap } from './utils';
 
 export type Connector = ReturnType<typeof createMembraneMarshall>;
 
 const TypeErrorCtor = TypeError;
-const WeakMapCtor = WeakMap;
-
-const evaluatorToBlueCreateHooksCallbackMap = toSafeWeakMap(
-    new WeakMapCtor<typeof eval, Connector>()
-);
-
-const evaluatorToRedCreateHooksCallbackMap = toSafeWeakMap(
-    new WeakMapCtor<typeof eval, Connector>()
-);
 
 const createMembraneMarshallSourceInStrictMode = `
 'use strict';
@@ -22,28 +12,12 @@ export function createBlueConnector(globalObject: typeof globalThis): Connector 
     if (typeof globalObject !== 'object' || globalObject === null) {
         throw new TypeErrorCtor('Missing globalObject.');
     }
-    const { eval: evaluator } = globalObject;
-    if (typeof evaluator !== 'function') {
-        throw new TypeErrorCtor('Missing evaluator function.');
-    }
-    let createHooksCallback = evaluatorToBlueCreateHooksCallbackMap.get(evaluator);
-    if (createHooksCallback === undefined) {
-        createHooksCallback = createMembraneMarshall(globalObject);
-        evaluatorToBlueCreateHooksCallbackMap.set(evaluator, createHooksCallback);
-    }
-    return createHooksCallback;
+    return createMembraneMarshall(globalObject);
 }
 
 export function createRedConnector(evaluator: typeof eval): Connector {
     if (typeof evaluator !== 'function') {
         throw new TypeErrorCtor('Missing evaluator function.');
     }
-    let createHooksCallback = evaluatorToRedCreateHooksCallbackMap.get(evaluator) as
-        | Connector
-        | undefined;
-    if (createHooksCallback === undefined) {
-        createHooksCallback = evaluator(createMembraneMarshallSourceInStrictMode)() as Connector;
-        evaluatorToRedCreateHooksCallbackMap.set(evaluator, createHooksCallback);
-    }
-    return createHooksCallback;
+    return evaluator(createMembraneMarshallSourceInStrictMode)() as Connector;
 }
