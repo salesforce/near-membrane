e79ec036afa9dc6ddb6e9791cf8c5588d5f63d5a
John-David Dalton <john.david.dalton@gmail.com>
2022-09-15 16:01:12 -0700

    chore: @W-11765740 cleanup scripts (#391)

--- karma.config.js
+++ karma.config.js
@@ -5,7 +5,7 @@
 const globby = require('globby');
 const istanbul = require('rollup-plugin-istanbul');
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
-const path = require('path');
+const path = require('node:path');
 
 process.env.CHROME_BIN = require('puppeteer').executablePath();
 
