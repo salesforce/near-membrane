1c9e467edf6e2c99c014bdcec5b13f78e1ab89d1
jdalton <john.david.dalton@gmail.com>
2023-03-23 11:20:44 -0700

    chore: @W-12740677 revert JSON to being non-remapped

--- packages/near-membrane-base/src/intrinsics.ts
+++ packages/near-membrane-base/src/intrinsics.ts
@@ -84,7 +84,7 @@ const ESGlobalKeys = [
 
     // *** 18.4 Other Properties of the Global Object
     // 'Atomics', // Remapped
-    // 'JSON', // Remapped
+    'JSON',
     'Math',
     'Reflect',
 
