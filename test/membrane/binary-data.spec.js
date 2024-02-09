b770b9dbd5f56b9c4b6f7867489c799db253a7e9
rwaldron <waldron.rick@gmail.com>
2024-02-09 12:02:01 -0500

    fix: do not remap crypto if remapTypedArrays is false

--- test/membrane/binary-data.spec.js
+++ test/membrane/binary-data.spec.js
@@ -43,15 +43,123 @@ describe('Blob', () => {
 });
 
 describe('Crypto', () => {
-    it('creates random values from typed arrays', () => {
+    it('creates random values from typed arrays', (done) => {
         const env = createVirtualEnvironment(window, {
-            endowments: Object.getOwnPropertyDescriptors({ expect }),
+            endowments: Object.getOwnPropertyDescriptors({ done, expect }),
         });
 
         env.evaluate(`
             expect(() => {
                 crypto.getRandomValues(new Uint8Array(1));
             }).not.toThrow();
+            async function f() {
+                const algorithm = { name: "RSA-OAEP" };
+                const data = new Uint8Array([255]);
+                const keyPair = await window.crypto.subtle.generateKey(
+                  {
+                    name: "RSA-OAEP",
+                    modulusLength: 4096,
+                    publicExponent: new Uint8Array([1, 0, 1]),
+                    hash: "SHA-256",
+                  },
+                  true,
+                  ["encrypt", "decrypt"],
+                );
+
+                const encrypted = await crypto.subtle.encrypt(
+                  algorithm,
+                  keyPair.publicKey,
+                  data
+                );
+
+                const decrypted = await crypto.subtle.decrypt(
+                  algorithm,
+                  keyPair.privateKey,
+                  encrypted
+                );
+            }
+
+            f().then(done);
+        `);
+    });
+    it('works when typed arrays are not remapped', (done) => {
+        const env = createVirtualEnvironment(window, {
+            endowments: Object.getOwnPropertyDescriptors({ done, expect }),
+            remapTypedArrays: false,
+        });
+
+        env.evaluate(`
+            expect(() => {
+                crypto.getRandomValues(new Uint8Array(1));
+            }).not.toThrow();
+            async function f() {
+                const algorithm = { name: "RSA-OAEP" };
+                const data = new Uint8Array([255]);
+                const keyPair = await window.crypto.subtle.generateKey(
+                  {
+                    name: "RSA-OAEP",
+                    modulusLength: 4096,
+                    publicExponent: new Uint8Array([1, 0, 1]),
+                    hash: "SHA-256",
+                  },
+                  true,
+                  ["encrypt", "decrypt"],
+                );
+
+                const encrypted = await crypto.subtle.encrypt(
+                  algorithm,
+                  keyPair.publicKey,
+                  data
+                );
+
+                const decrypted = await crypto.subtle.decrypt(
+                  algorithm,
+                  keyPair.privateKey,
+                  encrypted
+                );
+            }
+
+            f().then(done);
+        `);
+    });
+    it('ignores the presense of crypto in endowments if remapTypedArrays is false', (done) => {
+        const env = createVirtualEnvironment(window, {
+            endowments: Object.getOwnPropertyDescriptors({ Crypto, crypto, done, expect }),
+            remapTypedArrays: false,
+        });
+
+        env.evaluate(`
+            expect(() => {
+                crypto.getRandomValues(new Uint8Array(1));
+            }).not.toThrow();
+            async function f() {
+                const algorithm = { name: "RSA-OAEP" };
+                const data = new Uint8Array([255]);
+                const keyPair = await window.crypto.subtle.generateKey(
+                  {
+                    name: "RSA-OAEP",
+                    modulusLength: 4096,
+                    publicExponent: new Uint8Array([1, 0, 1]),
+                    hash: "SHA-256",
+                  },
+                  true,
+                  ["encrypt", "decrypt"],
+                );
+
+                const encrypted = await crypto.subtle.encrypt(
+                  algorithm,
+                  keyPair.publicKey,
+                  data
+                );
+
+                const decrypted = await crypto.subtle.decrypt(
+                  algorithm,
+                  keyPair.privateKey,
+                  encrypted
+                );
+            }
+
+            f().then(done);
         `);
     });
     it('works when typed arrays are not remapped', () => {
