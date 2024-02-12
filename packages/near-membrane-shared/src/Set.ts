752b66101b80fa1933a275667482bc71b42b9dac
rwaldron <waldron.rick@gmail.com>
2024-02-12 12:29:19 -0500

    fix: do not remap crypto, Crypto and SubtleCrypto if remapTypedArrays is false

--- packages/near-membrane-shared/src/Set.ts
+++ packages/near-membrane-shared/src/Set.ts
@@ -4,6 +4,6 @@ export const SetCtor = Set;
 
 const { prototype: SetProto } = SetCtor;
 
-export const { add: SetProtoAdd, values: SetProtoValues } = SetProto;
+export const { add: SetProtoAdd, has: SetProtoHas, values: SetProtoValues } = SetProto;
 
 export const SetProtoSizeGetter = ObjectLookupOwnGetter(SetProto, 'size')!;
